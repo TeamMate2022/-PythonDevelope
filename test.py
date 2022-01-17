3c64efc7fe1fa0041def3e85629d2b9d54ac748f
Amirhosein.syh <Amirhosein.syh@gmail.com>
2022-01-17 19:22:35 +0330

    check this out create csv file and update the csv files

--- test.py
+++ test.py
@@ -1,56 +1,110 @@
 
 
 import os
-import pandas as pd
 import csv
-
+import shutil
+import datetime
 WRITE = 'w'
 
 CSV_FILE = os.path.dirname(__file__)
 def make_csv_file (pros):
-    for pro in pros:
-        path = CSV_FILE  +'\\' + pro 
+    try:
+
+        path = CSV_FILE  + '\\' + pros['username']
         os.mkdir(path)
-        path + '\\' + pro + '.csv'
-
-        # with open(csv_path,'w') as file:
-        #     writer = csv.writer(file)
-        #     writer.writerow(["url"])
-make_csv_file("pros")
-
-
-
-def add_new_row(pros):
-        for pro in pros:
-            path = CSV_FILE  +'\\' + pro 
-            csv_path = path + '\\' + pro + '.csv'
-            new_row = {'url':'12345678910'}
+        path + '\\' + pros['username'] + '.csv'
+    except FileExistsError:
+        # path = CSV_FILE  + '\\' + pros['username']
+        # # shutil.rmtree(path)
+        # os.mkdir(path)
+        # path + '\\' + pros['username'] + '.csv'
+        pass
+        
+    # with open(csv_path,'w') as file:
+    #     writer = csv.writer(file)
+    #     writer.writerow(["url"])
 
 
-            urls=list(new_row.values())
-            print(urls)
-            url_path = path + '\\' + urls[0] + '.csv'
-            # os.mkdir(url_path)
-            # print(url_path)
-            
-            headers = list(new_row.keys())
-             
-            # if action == WRITE:
-            with open(csv_path, 'w', newline= '') as csv_file:   
-                writer = csv.DictWriter(csv_file,fieldnames = headers)
-                writer.writeheader()  
-                writer.writerow(new_row) 
-                with open (url_path , "w" , newline = "") as file:
-                    header = ["url" , "type" , "likes" , "views" , "time"]
-                    writer = csv.DictWriter(file,fieldnames = header)
-                    writer.writeheader()  
-                    writer.writerow(new_row) 
+
+WRITE = 'w'
+APPEND = 'a'
+
+def add_new_row(pros, action):
+
+        path = CSV_FILE  +'\\' + pros['username']
+        
+        csv_path = path + '\\' + pros['username'] + '.csv'
+        url_path = path + '\\' + pros['url'] + '.csv'
+
+        new_row = pros['url']
+
+        print(type(new_row))
+        # os.mkdir(url_path)
+        # print(url_path)
+
+        headers = ['url']
+
+        print(headers)
+        
+        header = list(pros.keys())
+
+        if action == WRITE:
+            with open(csv_path, 'w', newline= '') as csv_file:
                 
-            # elif action == APPEND:
-                # with open(csv_path, 'a', newline= '') as csv_file:       
-                #     writer = csv.DictWriter(csv_file,fieldnames = headers )
-                #     writer.writerow(new_row)
-            # df = pd.read_csv(csv_path)
-            # df = df.append(new_row , ignore_index=True)
-            # print(df)
-add_new_row("pros")
+                writer = csv.DictWriter(csv_file,fieldnames = headers)
+                
+                writer.writeheader()
+
+                writer.writerow({'url': pros['url']})
+
+            with open (url_path , "w" , newline = "") as file:
+                writer = csv.DictWriter(file,fieldnames = header)
+                writer.writeheader()
+                writer.writerow(pros)
+                
+        if action == APPEND:
+            with open(csv_path, 'a', newline= '') as csv_file:
+                
+                writer = csv.DictWriter(csv_file,fieldnames = headers)
+                
+                # writer.writeheader()
+
+                writer.writerow({'url': pros['url']})
+
+            with open (url_path , "a" , newline = "") as file:
+                header = list(pros.keys())
+                print(header)
+                writer = csv.DictWriter(file,fieldnames = header)
+                # writer.writeheader()
+                writer.writerow(pros)
+         
+
+info = {'username' : 'amir',
+        "url" : 'hawwwfwqefef' ,
+        "type" : 'image',
+        "likes" : 42323,
+        "views" : 2534237643676,
+        "time" :  datetime.datetime.now().now().strftime("%X")}
+
+
+
+make_csv_file(info)
+add_new_row(info,APPEND)
+
+
+
+
+
+
+
+
+
+
+
+# elif action == APPEND:
+    # with open(csv_path, 'a', newline= '') as csv_file:
+    #     writer = csv.DictWriter(csv_file,fieldnames = headers )
+    #     writer.writerow(new_row)
+# df = pd.read_csv(csv_path)
+# df = df.append(new_row , ignore_index=True)
+# print(df)
